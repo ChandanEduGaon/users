c8b29fd07c21f14d532027d1314a49af2f349689
Chandan Kumar <singh963130@gmail.com>
2023-05-28 16:38:32 +0530

    db add

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
+
 const port = process.env.PORT || 3000;
+const pool = mysql.createPool({
+  host: "0.0.0.0",
+  user: "root",
+  password: "",
+  database: "sql12621944",
+});
 app.get("/", (req, res) => {
-  res.send("Chandan Singh")
+  pool.getConnection((err, connection) => {
+    if (err) {
+      res.status(500).json({ error: "Error connecting to the database" });
+    } else {
+      connection.query("SELECT * FROM teachers", (error, results) => {
+        connection.release(); // Release the connection back to the pool
+        if (error) {
+          res.status(500).json({ error: "Error executing the query" });
+        } else {
+          res.json(results);
+        }
+      });
+    }
+  });
 });
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);

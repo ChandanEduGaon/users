ea1373616215be6fbcebe4ea9eecb192e884dafa
Chandan Kumar <singh963130@gmail.com>
2023-05-28 16:09:23 +0530

    mysql removed

--- index.js
+++ index.js
@@ -1,29 +1,8 @@
 const express = require("express");
-const mysql = require("mysql");
 const app = express();
-
 const port = process.env.PORT || 3000;
-const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "elearning",
-});
-app.get("/api/users", (req, res) => {
-  pool.getConnection((err, connection) => {
-    if (err) {
-      res.status(500).json({ error: "Error connecting to the database" });
-    } else {
-      connection.query("SELECT * FROM users", (error, results) => {
-        connection.release(); // Release the connection back to the pool
-        if (error) {
-          res.status(500).json({ error: "Error executing the query" });
-        } else {
-          res.json(results);
-        }
-      });
-    }
-  });
+app.get("/", (req, res) => {
+  res.send("Chandan Singh")
 });
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);

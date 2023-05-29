1e1d733871fd64dc0310492ff5e87f5de422e6b0
Chandan Kumar <singh963130@gmail.com>
2023-05-29 15:13:08 +0530

    Cors added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mysql = require("mysql");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
 
 const port = process.env.PORT || 3000;
 const pool = mysql.createPool({

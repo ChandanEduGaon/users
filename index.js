e52a6755716d9a7769e34c470d1808bc76569b83
Chandan Kumar <singh963130@gmail.com>
2023-06-06 15:08:41 +0530

    changed

--- index.js
+++ index.js
@@ -1,33 +1,17 @@
 const express = require("express");
-const mysql = require("mysql");
 const cors = require("cors");
 const app = express();
-
+app.use(express.urlencoded({ extended: false }))
 app.use(cors());
 
 const port = process.env.PORT || 3000;
-const pool = mysql.createPool({
-  host: "sql12.freesqldatabase.com",
-  user: "sql12621944",
-  password: "CBh1yLwf48",
-  database: "sql12621944",
-});
 app.get("/", (req, res) => {
-  pool.getConnection((err, connection) => {
-    if (err) {
-      res.status(500).json({ error: "Error connecting to the database" });
-    } else {
-      connection.query("SELECT * FROM users", (error, results) => {
-        connection.release(); // Release the connection back to the pool
-        if (error) {
-          res.status(500).json({ error: "Error executing the query" });
-        } else {
-          res.json(results);
-        }
-      });
-    }
-  });
+  res.status(200).send('OK');
 });
+
+app.post("/register", (req, res) => {
+  res.status(200).send(req.body)
+})
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
 });

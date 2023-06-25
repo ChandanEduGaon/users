7cefb262fdd177763f498d976fe86f16acc32883
Chandan Kumar <singh963130@gmail.com>
2023-06-25 16:41:04 +0530

    Created

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var multer = require("multer");
+const cors = require("cors");
 var upload = multer();
 var app = express();
+const port = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(upload.array());
 app.use(express.static("public"));
+app.use(cors({ origin: process.env.REMOTE_CLIENT_APP, credentials: true }));
 
 const users = [
   { name: "Raushan", age: 19, email: "raushan@example.example" },
@@ -17,7 +20,9 @@ const users = [
   { name: "Amar", age: 20, email: "amar@example.example" },
   { name: "Ramu", age: 19, email: "ramu@example.example" },
 ];
-
+app.get("/", function (req, res) {
+  res.send("<h1>Welcome</h1>");
+});
 app.get("/delete_user/:id", function (req, res) {
   delete users[req.params.id];
   res.send("User deleted of id : " + req.params.id);
@@ -37,14 +42,14 @@ app.get("/user/:id", function (req, res) {
   } else if (req.params.id < 0) {
     res.send("Request is not valid");
   } else {
-    res.send(users[req.params.id]);
+    res.send(JSON.stringify(users[req.params.id]));
   }
 });
 app.get("/users", function (req, res) {
-  res.send(users);
+  res.send(JSON.stringify(users));
 });
 app.get("/*", function (req, res) {
   res.send("Search like '/user/:id'");
 });
 
-app.listen(3000);
+app.listen(port);
